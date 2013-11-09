e5f712e0a38f44387f0397d072c130abf9f932be
Hassan Munir <hassanmunir@live.com>
2013-11-09 14:48:06 +0000

    Created a simple menu simulation which supports horizontal and vertical scrolling

---
+++ SmartWatch.Menu/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System.Threading;
+using System.Windows;
+using SmartWatch.Core.Gestures;
+using SmartWatch.Core.Mocks;
+
+namespace SmartWatch.Menu
+{
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var gestures = new MockGestures();
+            gestures.ScrollVertical += gestures_ScrollVertical;
+            gestures.ScrollHorizontal += gestures_ScrollHorizontal;
+        }
+
+        private void gestures_ScrollHorizontal(object sender, GestureParameters e)
+        {
+            var shouldScrollLeft = (ScrollViewerControl.ScrollableWidth > 0) &&
+                                   (ScrollViewerControl.HorizontalOffset > 0);
+
+            var shouldScrollRight = (ScrollViewerControl.ScrollableWidth > 0) &&
+                                    ((ScrollViewerControl.HorizontalOffset + ScrollViewerControl.ViewportWidth) <
+                                     ScrollViewerControl.ExtentWidth);
+
+            if (shouldScrollLeft)
+            {
+                while (ScrollViewerControl.HorizontalOffset > 0)
+                {
+                    ScrollHorizontally(-10);
+                }
+            }
+            else if (shouldScrollRight)
+            {
+                while ((ScrollViewerControl.HorizontalOffset + ScrollViewerControl.ViewportWidth) <
+                       ScrollViewerControl.ExtentWidth)
+                {
+                    ScrollHorizontally(10);
+                }
+            }
+        }
+
+        private void ScrollHorizontally(int change)
+        {
+            var offset = ScrollViewerControl.HorizontalOffset + change;
+            ScrollViewerControl.Dispatcher.Invoke(() => ScrollViewerControl.ScrollToHorizontalOffset(offset));
+            Thread.Sleep(10);
+        }
+
+        private void gestures_ScrollVertical(object sender, GestureParameters e)
+        {
+            var shouldScrollUp = (ScrollViewerControl.ScrollableHeight > 0) && (ScrollViewerControl.VerticalOffset > 0);
+
+            var shouldScrollDown = (ScrollViewerControl.ScrollableHeight > 0) &&
+                                   ((ScrollViewerControl.VerticalOffset + ScrollViewerControl.ViewportHeight) <
+                                    ScrollViewerControl.ExtentHeight);
+
+            if (shouldScrollUp)
+            {
+                while (ScrollViewerControl.VerticalOffset > 0)
+                {
+                    ScrollVertically(-10);
+                }
+
+            }
+            else if (shouldScrollDown)
+            {
+                while ((ScrollViewerControl.VerticalOffset + ScrollViewerControl.ViewportHeight) <
+                       ScrollViewerControl.ExtentHeight)
+                {
+                    ScrollVertically(10);
+                }
+            }
+        }
+
+        private void ScrollVertically(int change)
+        {
+            var offset = ScrollViewerControl.VerticalOffset + change;
+            ScrollViewerControl.Dispatcher.Invoke(() => ScrollViewerControl.ScrollToVerticalOffset(offset));
+            Thread.Sleep(10);
+        }
+    }
+}
