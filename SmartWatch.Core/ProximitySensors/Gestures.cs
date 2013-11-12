98b499177160ef89bc7db7ecbda4fe9099d52adb
Hassan Munir <hassanmunir@live.com>
2013-11-12 12:59:53 +0000

    Gesture recognition

--- SmartWatch.Core/ProximitySensors/Gestures.cs
+++ SmartWatch.Core/ProximitySensors/Gestures.cs
@@ -1,20 +1,116 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Security.Cryptography;
 using SmartWatch.Core.Gestures;
+using SmartWatch.Core.Mocks;
 
 namespace SmartWatch.Core.ProximitySensors
 {
     public class Gestures : IGestures
     {
+        List<int> list = new List<int>();
+
         public Gestures()
         {
-            var arduino = new Arduino();
+            //var arduino = new Arduino();
+            var arduino = new ArduinoMock();
             arduino.DataRecieved += arduino_DataRecieved;
             arduino.OpenConnection();
         }
 
+        List<int> detect(List<int> list, List<int> gradient)
+        {
+            var min = 0;
+            var max = 100;
+
+            // gesture is only valid if the hand is moving half of the maximun distance
+
+            var valid_dist = (max - min)/2;
+
+            var number = 5;
+
+            var b = 0;
+
+            int ii = 0;
+            int jj = 0;
+
+            for (int i = 0; i < gradient.Count - number; i++)
+            {
+                for (int j = 0; j < number; j++)
+                {
+                    if (gradient[i + j] < 0)
+                    {
+                        b = 1;
+                        jj = j;
+                        break;
+                    }
+                }
+
+                if (b == 1)
+                {
+                    ii = i;
+                    break;
+                }
+            }
+
+            if (b == 0 && gradient.Count >= number)
+            {
+                Debug.WriteLine("scroll to the right");
+
+                foreach (int item in list)
+                {
+                    Debug.WriteLine(item);
+                }
+
+                Debug.WriteLine("11111111111111111");
+                OnScrollHorizontal(new ScrollParameters(new Point(0,50), new Point(100, 50) ));
+
+                list.RemoveRange(0, ii + jj);
+
+                foreach (int item in list)
+                {
+                    Debug.WriteLine(item);
+                }
+            }
+
+            return list;
+        }
+
+        // apply filter to the list
+        // gradient filter is applied at the moment
+        List<int> process(List<int> list)
+        {
+            List<int> filtered_list = new List<int>();
+
+            int[] mask = new int[] {-1, 1};
+
+            int val;
+
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                val = mask[0] * list[i] + mask[1] * list[i + 1];
+                
+                filtered_list.Add(val);
+            }
+
+            return filtered_list;
+        }
+
         void arduino_DataRecieved(object sender, int e)
         {
-            throw new NotImplementedException();
+
+            list.Add(e);
+
+            List<int> filtered_list = process(list);
+
+            // every time theres a new data, it tries to do a detection
+            list = detect(list, filtered_list);
+
+            Debug.WriteLine("start");
+
+
         }
 
 
