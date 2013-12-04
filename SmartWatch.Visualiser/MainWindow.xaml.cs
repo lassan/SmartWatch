56669632b2e97caa3b0e1b8f6807a0ee65fb6f6b
Hassan Munir <hassanmunir@live.com>
2013-12-04 13:03:40 +0000

    New application to visualise the output from the sensors in real time

---
+++ SmartWatch.Visualiser/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using SmartWatch.Core;
+using SmartWatch.Core.Mocks;
+
+namespace SmartWatch.Visualiser
+{
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            var arduino = new ArduinoMock();
+            //var arduino = new Arduino("COM3");
+            ViewModel = new ViewModel(arduino);
+            InitializeComponent();
+        }
+
+        public ViewModel ViewModel { get; set; }
+    }
+}
---
+++ SmartWatch.Visualiser/ViewModel.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using SmartWatch.Core;
+using SmartWatch.Visualiser.Annotations;
+using WobbrockLib;
+
+namespace SmartWatch.Visualiser
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        #region View Properties
+
+        public int Sensor1Proximity
+        {
+            get { return _sensor1Proximity; }
+            set
+            {
+                if (value == _sensor1Proximity) return;
+                _sensor1Proximity = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int Sensor2Proximity
+        {
+            get { return _sensor2Proximity; }
+            set
+            {
+                if (value == _sensor2Proximity) return;
+                _sensor2Proximity = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int Sensor3Proximity
+        {
+            get { return _sensor3Proximity; }
+            set
+            {
+                if (value == _sensor3Proximity) return;
+                _sensor3Proximity = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        private int _sensor1Proximity;
+        private int _sensor2Proximity;
+        private int _sensor3Proximity;
+
+        public ViewModel(IArduino arduino)
+        {
+            arduino.Connect();
+            arduino.DataRecieved += ArduinoDataRecievedHandler;
+        }
+
+        private void ArduinoDataRecievedHandler(object sender, List<TimePointF> e)
+        {
+            Sensor1Proximity = (int) e[0].X;
+            Sensor2Proximity = (int) e[1].X;
+            Sensor3Proximity = (int) e[2].X;
+        }
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}
